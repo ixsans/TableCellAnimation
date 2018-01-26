0c1bc7761724934e3df812d2489905d10482e762
Ikhsanudin Hakim <ikhsanudinhakim@gmail.com>
2018-01-26 23:24:24 +0700

    initial commit

---
+++ TableCellAnimation/ArticleTableViewController.swift
@@ -0,0 +1,82 @@
+//
+//  ArticleTableViewController.swift
+//  TableCellAnimation
+//
+//  Created by Simon Ng on 3/10/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewController: UITableViewController {
+    
+    let postTitles = ["Use Background Transfer Service To Download File in Background",
+                      "Face Detection in iOS Using Core Image",
+                      "Building a Speech-to-Text App Using Speech Framework in iOS 10",
+                      "Building Your First Web App in Swift Using Vapor",
+                      "Creating Gradient Colors Using CAGradientLayer",
+                      "A Beginner's Guide to CALayer"];
+    let postImages = ["imessage-sticker-pack", "face-detection-featured", "speech-kit-featured", "vapor-web-framework", "cagradientlayer-demo", "calayer-featured"];
+
+    var postShown = [Bool](repeating: false, count: 6)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.estimatedRowHeight = 258.0
+        tableView.rowHeight = UITableViewAutomaticDimension
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postTitles.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ArticleTableViewCell
+        
+        // Configure the cell...
+        cell.titleLabel.text = postTitles[(indexPath as NSIndexPath).row]
+        cell.postImageView.image = UIImage(named: postImages[(indexPath as NSIndexPath).row])
+
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        
+        if postShown[indexPath.row] {
+            return
+        }
+        
+        // Indicate the post has been displayed, so the animation won't be displayed again
+        postShown[indexPath.row] = true
+        //FADE
+//        cell.alpha = 0
+//        UIView.animate(withDuration: 1.0, animations: {cell.alpha = 1})
+        
+        //ROTATION
+//        let rotationAngleRadians = 90.0 * CGFloat(M_PI/180.0)
+//        let rotatitonTransform = CATransform3DMakeRotation(rotationAngleRadians, 0, 0, 1)
+//        cell.layer.transform = rotatitonTransform
+//        UIView.animate(withDuration: 1.0, animations: { cell.layer.transform = CATransform3DIdentity})
+        
+        // TRANSLATION
+        let rotatitonTransform = CATransform3DTranslate(CATransform3DIdentity, -500, 100, 0)
+        cell.layer.transform = rotatitonTransform
+        UIView.animate(withDuration: 1.0, animations: { cell.layer.transform = CATransform3DIdentity})
+
+    }
+
+}
